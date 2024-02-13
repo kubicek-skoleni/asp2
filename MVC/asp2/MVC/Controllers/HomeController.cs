6564f8744356fb45d2685fae4f312fad2e3f37d8
LukasKubice <lukaskubicek@gmail.com>
2024-02-13 10:38:40 +0100

    mvc hello world + razor

--- MVC/asp2/MVC/Controllers/HomeController.cs
+++ MVC/asp2/MVC/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace MVC.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
