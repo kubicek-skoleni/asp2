592d5e1cfccf972f4f5a87bcb87b06b891599d6c
LukasKubice <lukaskubicek@gmail.com>
2024-02-13 14:54:32 +0100

    pripojeni do db

--- MVC/asp2/InsuranceCorp.Blazor/Program.cs
+++ MVC/asp2/InsuranceCorp.Blazor/Program.cs
@@ -1,10 +1,13 @@
 using InsuranceCorp.Blazor.Components;
+using InsuranceCorp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<InsCorpDbContext>();
 
 var app = builder.Build();
 
