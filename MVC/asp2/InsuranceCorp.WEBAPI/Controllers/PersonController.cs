343f759b4dfb9ba2417acbbb4c7477fc2ac89aaf
LukasKubice <lukaskubicek@gmail.com>
2024-02-14 12:42:31 +0100

    person list api

---
+++ MVC/asp2/InsuranceCorp.WEBAPI/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using InsuranceCorp.Data;
+using InsuranceCorp.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceCorp.WEBAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PersonController : ControllerBase
+	{
+		private readonly InsCorpDbContext db;
+
+		public PersonController(InsCorpDbContext db) 
+		{
+			this.db = db;
+		}
+
+		//[Route("list")]
+		[HttpGet("list")]
+		public ActionResult<List<Person>> List(int start, int take) 
+		{
+			return db.Persons.Skip(start).Take(take).ToList();
+		}
+	}
+}
